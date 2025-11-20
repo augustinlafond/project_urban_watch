90f3b54a714917e75f46757339b530a5eaad5975
Kevin Tcha <kevintcha.lshmg@gmail.com>
2025-11-20 15:11:21 +0100

    start mlfl

--- urban_watch/ml_logic/registry.py
+++ urban_watch/ml_logic/registry.py
@@ -7,10 +7,8 @@ from mlflow.tracking import MlflowClient
 def save_model(model, model_name='model'):
 
     if MODEL_TARGET == "mlflow":
-        mlflow.sklearn.log_model(model, artifact_path=MLFLOW_MODEL_NAME)
+        mlflow.     .log_model(model=model,
+                               artifact_path=MLFLOW_MODEL_NAME,
+                               registered_model_name=MLFLOW_MODEL_NAME)
         print("model logged to mlflow")
         return
-
-    #ici la local save
-    model_dir = os.path.join(LOCAL_REGISTRY_PATH, 'models')
-    os.makedirs(model_dir)
