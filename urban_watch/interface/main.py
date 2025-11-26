9712e2ee582c0fe3766d7cca6251915409a8e376
Kevin Tcha <kevintcha.lshmg@gmail.com>
2025-11-26 11:10:29 +0100

    apply black + package sorting

--- urban_watch/interface/main.py
+++ urban_watch/interface/main.py
@@ -8,23 +8,45 @@ This file ONLY coordinates the high-level pipeline steps:
 - Evaluation (soon)
 - Prediction (later)
 
-No detailed logic lives here.
 """
 
-from sentinelhub import SHConfig
-from dotenv import load_dotenv
+
+# Standard library
 import os
+
+# Third-party libraries
 import numpy as np
 from colorama import Fore, Style
+from sentinelhub import SHConfig
+
+# Project-specific imports
+from urban_watch.params import SH_CLIENT_ID, SH_CLIENT_SECRET
 from urban_watch.ml_logic.data import get_data, load_data, RAW_DATA_DIR
 from urban_watch.ml_logic.preprocessing import apply_preproc_X_y, preprocess_image
-from urban_watch.ml_logic.labels import get_bbox_from_features, bbox_to_wgs84, tile_name_from_bbox_wgs84, get_label_array, load_labels_y, LABELS_DIR
-from urban_watch.ml_logic.model import split_data, train_logreg,train_random_forest, train_xgb, evaluate_model
-from urban_watch.ml_logic.registry import save_model, save_results, mlflow_run, load_model
-from urban_watch.params import *
+from urban_watch.ml_logic.labels import (
+    get_bbox_from_features,
+    bbox_to_wgs84,
+    tile_name_from_bbox_wgs84,
+    get_label_array,
+    load_labels_y,
+    LABELS_DIR,
+)
+from urban_watch.ml_logic.model import (
+    split_data,
+    train_logreg,
+    train_random_forest,
+    train_xgb,
+    evaluate_model,
+)
+from urban_watch.ml_logic.registry import (
+    save_model,
+    save_results,
+    mlflow_run,
+    load_model,
+)
+
 
 #   MAIN FUNCTIONS CALLED BY CLI
-
 def full_preproc_pipeline():
     """
     Run the full preprocessing pipeline:
@@ -53,26 +75,35 @@ def full_preproc_pipeline():
 
     # Center of the bbox used for the train
     list_bbox_centers = [
-    (43.59533138728404, 6.944167292762329), # Cannes area, mix of urban/vegetation
-    (43.407067616126554, 6.522669188972711), # Var region / vegetation
-    (43.128306760274825, 6.121503808299507), # Hyère/vegetation
-    (43.13997422359963, 5.928698334059295), # North of Toulon
-    (43.23554616596631, 5.881493437321922), # Sainte-Baume regional park / vegetation
-    (43.219759411248, 5.7558111438277315), # Le Castellet (Var) / fields / vegetation / housing
-    (43.58587029251304, 5.4526213864786985), # Au dessus d'AIx en Pce/champs,route,maisons
-    (43.293089430781095, 5.389109345966383), # Marseille
-    (43.5306142665247, 5.438947178151266), # Aix
-    (43.53546797333638, 1.9686993230644818), # East of Toulouse / fields
-    (48.83661245140578, 2.409813543996677), # East Paris/urban + park
-    (48.115035919487454, -1.6830147555182722), # Rennes
-    (46.702018498290286, 0.7668558109521145), # Fields near Poitiers
-    (47.49143988878275, 2.0914005645009692), # Forest south of Orléans
-    (47.26267555373632, 4.060278597924947), # PNR Morvan
-    (44.802566728154716, 4.375843852678453), # Ardèche
-    (43.88785890061108, 0.5595568148255528), # Southwest / fields
-    (45.1278081381777, -1.0510523421771456), # Médoc
-    (43.51973871166271, 0.9356166219979338) # Southwest fields
- ]
+        (43.59533138728404, 6.944167292762329),  # Cannes area, mix of urban/vegetation
+        (43.407067616126554, 6.522669188972711),  # Var region / vegetation
+        (43.128306760274825, 6.121503808299507),  # Hyère/vegetation
+        (43.13997422359963, 5.928698334059295),  # North of Toulon
+        (
+            43.23554616596631,
+            5.881493437321922,
+        ),  # Sainte-Baume regional park / vegetation
+        (
+            43.219759411248,
+            5.7558111438277315,
+        ),  # Le Castellet (Var) / fields / vegetation / housing
+        (
+            43.58587029251304,
+            5.4526213864786985,
+        ),  # Au dessus d'AIx en Pce/champs,route,maisons
+        (43.293089430781095, 5.389109345966383),  # Marseille
+        (43.5306142665247, 5.438947178151266),  # Aix
+        (43.53546797333638, 1.9686993230644818),  # East of Toulouse / fields
+        (48.83661245140578, 2.409813543996677),  # East Paris/urban + park
+        (48.115035919487454, -1.6830147555182722),  # Rennes
+        (46.702018498290286, 0.7668558109521145),  # Fields near Poitiers
+        (47.49143988878275, 2.0914005645009692),  # Forest south of Orléans
+        (47.26267555373632, 4.060278597924947),  # PNR Morvan
+        (44.802566728154716, 4.375843852678453),  # Ardèche
+        (43.88785890061108, 0.5595568148255528),  # Southwest / fields
+        (45.1278081381777, -1.0510523421771456),  # Médoc
+        (43.51973871166271, 0.9356166219979338),  # Southwest fields
+    ]
 
     ########## Get the features X ##########
     ########################################
@@ -98,23 +129,22 @@ def full_preproc_pipeline():
 
     print(Fore.CYAN + "✅ Labels are loaded \n" + Style.RESET_ALL)
 
-    X_preproc, y_preproc = apply_preproc_X_y(X,y)
+    X_preproc, y_preproc = apply_preproc_X_y(X, y)
 
     print("✅ preprocess() done \n")
 
     return X_preproc, y_preproc
 
 
-
 MODEL_DISPATCHER = {
     "logreg": train_logreg,
     "random_forest": train_random_forest,
-    "xgb": train_xgb
+    "xgb": train_xgb,
 }
 
 
 @mlflow_run
-def train(X,y, model_type='logreg', **model_params):
+def train(X, y, model_type="logreg", **model_params):
     """
     - Train on the preprocessed X and y
     - Store training results and model weights
@@ -127,16 +157,9 @@ def train(X,y, model_type='logreg', **model_params):
 
     X_train, X_test, y_train, y_test = split_data(X, y, test_size=0.2)
 
-    model, train_metrics = trainer_fn(
-        X_train, y_train,
-        **model_params
-    )
+    model, train_metrics = trainer_fn(X_train, y_train, **model_params)
 
-    params = dict(
-        context="train",
-        model_type= model_type,
-        row_count=X_train.shape[0]
-    )
+    params = dict(context="train", model_type=model_type, row_count=X_train.shape[0])
 
     # Save results on MLFlow
     save_results(params=params, metrics=train_metrics)
@@ -147,22 +170,16 @@ def train(X,y, model_type='logreg', **model_params):
     return train_metrics
 
 
-
 @mlflow_run
-def evaluate(X,y,model_name="logreg_model", model_type="logreg", stage="Production"):
+def evaluate(X, y, model_name="logreg_model", model_type="logreg", stage="Production"):
     """
     Evaluate the performance of the latest production model on processed data
     Return a dictionary with the metrics (i.e. precision, recall, f1, accuracy)
     """
-    model = load_model(
-        model_name=model_name,
-        model_type=model_type,
-        stage=stage
-        )
+    model = load_model(model_name=model_name, model_type=model_type, stage=stage)
     if model is None:
         print(f"no model found: {model_name} ({stage})")
         return None
-
 
     X_train, X_test, y_train, y_test = split_data(X, y, test_size=0.2)
 
@@ -170,16 +187,15 @@ def evaluate(X,y,model_name="logreg_model", model_type="logreg", stage="Producti
 
     params = dict(
         context="evaluate",
-        model_name= model_name,
-        model_type= model_type,
+        model_name=model_name,
+        model_type=model_type,
         stage=stage,
-        row_count=X_test.shape[0]
-                   )
+        row_count=X_test.shape[0],
+    )
 
     save_results(params=params, metrics=metrics)
 
     print("✅ evaluate() done \n")
-
 
 
 def rebuild_prediction(y_pred, mask_valid, fill_value=-1):
@@ -196,7 +212,6 @@ def rebuild_prediction(y_pred, mask_valid, fill_value=-1):
     y_full[mask_valid] = y_pred
 
     return y_full
-
 
 
 def pred(X_pred, model):
