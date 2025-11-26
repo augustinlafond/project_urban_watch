72b487c6f87c0bfb1e289c47c75b960f49f047c7
Kevin Tcha <kevintcha.lshmg@gmail.com>
2025-11-26 11:10:11 +0100

    apply black + package sorting

--- urban_watch/api/fast_api.py
+++ urban_watch/api/fast_api.py
@@ -11,15 +11,21 @@ The API provides one main endpoint:
 - **/predict**: Fetches a satellite tile for coordinates and date, processes it, predicts urbanization, and returns results.
 """
 
+
+# Standard library
 from datetime import datetime
+
+# Third-party libraries
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from sentinelhub import SHConfig
-import numpy as np
+
+# Project-specific imports
+from urban_watch.params import SH_CLIENT_ID, SH_CLIENT_SECRET
 from urban_watch.ml_logic.data import download_sentinel_image, image_rgb
-from urban_watch.interface.main import pred
-from urban_watch.params import *
 from urban_watch.ml_logic.registry import load_model
+from urban_watch.interface.main import pred
+
 
 # Configure access to the SentinelHub API
 config = SHConfig()
@@ -38,25 +44,21 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 # ROOT
 @app.get("/")
 def root():
     return {"status": "UrbanWatch API running :coche_blanche:"}
 
+
 app.state.model = load_model(
-        model_name="random_forest_model",
-        model_type="RandomForest",
-        stage="Production"
-    )
+    model_name="random_forest_model", model_type="RandomForest", stage="Production"
+)
+
 
 # PREDICT ENDPOINT
 @app.get("/predict")
-def predict(
-    date: str,
-    lon: float,
-    lat: float,
-    size_km : float
-):
+def predict(date: str, lon: float, lat: float, size_km: float):
     """
     lon and lat are the coordinates in WGS84 format of the center of the bbox
     size_km is the size of the window, eg., 3 correspond to a window of 3kmx3km centered on lon/lat coordinates
@@ -67,7 +69,7 @@ def predict(
     except ValueError:
         return {
             "error": "Invalid date format. Expected YYYY-MM-DD",
-            "example": "2021-06-15"
+            "example": "2021-06-15",
         }
 
     ## download Sentinel-2 image
@@ -75,17 +77,16 @@ def predict(
         image_sat = download_sentinel_image(date, lon, lat, size_km, config)
     except Exception as e:
         return {"error": f"SentinelHub download failed: {str(e)}"}
-    
+
     ## Return RGB image
     rgb_image = image_rgb(image_sat)
 
     ## call the existing prediction function
-    y_pred_full, mean_urban_score = pred(X_pred=image_sat,
-                                         model=app.state.model)
+    y_pred_full, mean_urban_score = pred(X_pred=image_sat, model=app.state.model)
 
     ## API response
     return {
-        "urbanization_score": float(round(mean_urban_score,2)),
+        "urbanization_score": float(round(mean_urban_score, 2)),
         "prediction": y_pred_full.tolist(),
-        "rgb" : rgb_image.tolist()
+        "rgb": rgb_image.tolist(),
     }
