19010c742d513a5f9609216ea01312d7888ffde1
Kevin Tcha <kevintcha.lshmg@gmail.com>
2025-11-26 11:11:51 +0100

    apply black + package sorting

--- urban_watch/params.py
+++ urban_watch/params.py
@@ -7,14 +7,14 @@ SH_CLIENT_SECRET = os.environ.get("SH_CLIENT_SECRET")
 
 
 BUCKET_NAME = os.environ.get("BUCKET_NAME")
-GCP_PROJECT=os.environ.get("GCP_PROJECT")
+GCP_PROJECT = os.environ.get("GCP_PROJECT")
 
 
 MLFLOW_TRACKING_URI = os.environ.get("MLFLOW_TRACKING_URI")
 MLFLOW_EXPERIMENT = os.environ.get("MLFLOW_EXPERIMENT")
 MLFLOW_MODEL_NAME = os.environ.get("MLFLOW_MODEL_NAME")
 
-#le chemin est pas fait
+# le chemin est pas fait
 
 IMG_HEIGHT = 300
 IMG_WIDTH = 300
